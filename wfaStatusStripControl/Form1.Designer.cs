2f14eafae68aec33f68ae435f801338ca93d9fa7
Luke <P10363830@capita.co.uk>
2018-03-02 15:54:45 +0000

    +1

--- wfaStatusStripControl/Form1.Designer.cs
+++ wfaStatusStripControl/Form1.Designer.cs
@@ -1,4 +1,8 @@
-﻿namespace wfaStatusStripControl
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace wfaStatusStripControl
 {
     partial class Form1
     {
@@ -20,6 +24,21 @@
             base.Dispose(disposing);
         }
 
+        private void ToolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+            ToolStripLabel myHyperLinkLabel = sender as ToolStripLabel;
+            try
+            {
+                Process.Start(new ProcessStartInfo(myHyperLinkLabel.ToString()));
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("A problem occurred" + ex.Message);
+            }
+        }
+
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -28,12 +47,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 0);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 23);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.IsLink = true;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(149, 18);
+            this.toolStripStatusLabel1.Spring = true;
+            this.toolStripStatusLabel1.Text = "http://www.microsoft.com";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.ToolStripStatusLabel1_Click);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel2.Click += new System.EventHandler(this.toolStripStatusLabel2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private ToolStripProgressBar toolStripStatusLabel2;
     }
 }
 
--- wfaStatusStripControl/Form1.cs
+++ wfaStatusStripControl/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,17 @@ namespace wfaStatusStripControl
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void toolStripStatusLabel2_Click(object sender, EventArgs e)
+        {
+            
+            for (int counter = 0; counter <= 95; counter++)
+            {
+                this.toolStripStatusLabel2.ProgressBar.Value = counter;
+                System.Threading.Thread.Sleep(50);
+
+            }
         }
     }
 }
